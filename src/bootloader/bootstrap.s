da47ded49c0881c9da722c25260051fcf9ecdf33
Abhay V <abhay.v2024@gmail.com>
2025-03-09 22:36:18 +0530

    Print a welcome message on boot

--- src/bootloader/bootstrap.s
+++ src/bootloader/bootstrap.s
@@ -3,6 +3,9 @@ format binary
 
 org 0x7c00
 main:
+	mov sp, stack_end
+	mov bp, stack_end
+
 	mov ah, 05h
 	xor al, al
 	int 10h
@@ -17,28 +20,58 @@ main:
 	mov dl, 0
 	int 10h
 
-	mov ecx, msg
-	mov ax, cx
-	shr ecx, 16
-	mov dx, cx
-	mov cx, 0xffff
-	div cx
+	; mov ecx, msg
+	; mov ax, cx
+	; shr ecx, 16
+	; mov dx, cx
+	; mov cx, 0xffff
+	; div cx
 
-	mov ds, ax
-	mov si, dx
+	; mov ds, ax
+	; mov si, dx
 
-	mov ah, 0ah
-	mov al, [msg]
-	xor bh, bh
-	mov cx, 5
-	int 10h
-	
+	; mov ah, 0ah
+	; mov al, [msg]
+	; xor bh, bh
+	; mov cx, 5
+	; int 10h
+
+	xor cl, cl
 
 .loop:
+	push cx
+
+	mov bx, msg
+	mov si, cx
+
+	mov ah, 0ah
+	mov al, [bx + si]
+	xor bh, bh
+	mov cx, 1
+	int 10h
+
+	pop cx
+	
+	inc cl
+
+	mov ah, 02h
+	xor bh, bh
+	mov dh, 0
+	mov dl, cl
+	int 10h
+
+	cmp cl, len
+	jl .loop
+	
+.exit:
 	
 	
 
-msg: db "Hello OS!", 10
+msg: db "Hello OS!"
 len = $ - msg
+
+stack_begin: 
+	rb 100
+stack_end:
 	
 
