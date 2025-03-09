bb3b56a4b9f52e1d2519d4227c4de057d8d10cb4
Abhay V <abhay.v2024@gmail.com>
2025-03-10 01:48:46 +0530

    Test A20 line

--- src/bootloader/bootsector.s
+++ src/bootloader/bootsector.s
@@ -2,76 +2,98 @@ use16
 format binary
 
 org 0x7c00
+	
 main:
 	mov sp, stack_end
 	mov bp, stack_end
+	
+	call check_A20
 
-	mov ah, 05h
 	xor al, al
+	mov ah, 05h
 	int 10h
 
-	mov ah, 00h
 	mov al, 03h
+	mov ah, 00h
 	int 10h
 
-	mov ah, 02h
 	xor bh, bh
 	mov dh, 0
 	mov dl, 0
-	int 10h
-
-	; mov ecx, msg
-	; mov ax, cx
-	; shr ecx, 16
-	; mov dx, cx
-	; mov cx, 0xffff
-	; div cx
-
-	; mov ds, ax
-	; mov si, dx
-
-	; mov ah, 0ah
-	; mov al, [msg]
-	; xor bh, bh
-	; mov cx, 5
-	; int 10h
-
-	xor cl, cl
-
-.loop:
-	push cx
-
-	mov bx, msg
-	mov si, cx
-
-	mov ah, 0ah
-	mov al, [bx + si]
-	xor bh, bh
-	mov cx, 1
-	int 10h
-
-	pop cx
-	
-	inc cl
-
 	mov ah, 02h
-	xor bh, bh
-	mov dh, 0
-	mov dl, cl
 	int 10h
 
-	cmp cl, len
-	jl .loop
-	
-.exit:
-	
+	; xor cl, cl
+
+; .loop:
+; 	push cx
+; 
+; 	xor ax, ax
+; 	mov ds, ax
+; 
+; 	mov si, cx
+; 	mov bx, msg
+; 	mov al, [bx + si]
+; 	xor bh, bh
+; 	mov cx, 1
+; 	mov ah, 0ah
+; 	int 10h
+; 
+; 	pop cx
+; 	
+; 	inc cl
+; 
+; 	xor bh, bh
+; 	mov dh, 0
+; 	mov dl, cl
+; 	mov ah, 02h
+; 	int 10h
+; 
+; 	cmp cl, len
+; 	jl .loop
 	
 
+enable_A20:
+	jmp exit
+
+check_A20:
+	xor ax, ax
+	mov ds, ax
+	mov si, 0x7dfe
+	mov ax, [si]
+
+	mov bx, 0xffff
+	mov ds, bx
+	mov si, 0x7e0e
+
+	cmp ax, [si]
+
+	jne .return
+
+	xor ax, ax
+	mov es, ax
+	mov di, 0x7dfe
+ 	mov [di], WORD 0x6969
+	mov ax, [es:di]
+
+	cmp ax, [si]
+
+	jne .return
+
+	call enable_A20
+	
+.return:
+	ret
+
+
+exit:
+
+	
 msg: db "Hello OS!"
 len = $ - msg
-
+	
 stack_begin: 
-	rb 100
+	rb 200
 stack_end:
 	
 
